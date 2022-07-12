3a0c7de1af917bd74bd15a177085f17a2da34f0f
´Peco´ <pecohernandez6@gmail.com>
2022-07-12 20:07:58 -0300

    Se realizo el punto 1

---
+++ PuntosTp2/Punto-1.js
@@ -0,0 +1,8 @@
+//Punto 1
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad >= 18) {
+  document.write("Ya puedes conducir");
+} else {
+  document.write("No tenes la edad permitida para conducir");
+}
