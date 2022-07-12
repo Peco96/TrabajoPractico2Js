4aacdc80c1c4d8e3666eafe46ad6cadc94e7a50a
´Peco´ <pecohernandez6@gmail.com>
2022-07-12 20:50:37 -0300

    punto 2 completo

---
+++ PuntosTp2/Punto-2.js
@@ -0,0 +1,21 @@
+//Punto2
+let nota = parseInt(prompt("Ingresar nota (número del 1-10)"));
+
+
+if ((nota == 0) || (nota == 1 )|| (nota == 2)){
+    alert("Muy deficiente")
+}else if ((nota == 3) || (nota == 4)){
+   alert("Insuficiente");
+}else if ((nota == 5) || (nota == 6)){
+    alert("Suficiente");
+}else if (nota == 7){
+    alert("Bien");
+}else if ((nota == 8) || (nota == 9)){
+    alert("Notable");
+}else if (nota == 10){
+    alert("Sobresaliente");
+}
+else{
+    document.write("número erróneo ");
+    document.write("Introduce un número válido"); 
+}
